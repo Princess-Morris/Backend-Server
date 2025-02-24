28f53ae38332c45a218a74f04c467d5d51cd6bcc
Princess-Morris <okpataprincessmorris3@gmail.com>
2025-02-24 09:26:39 +0100

    Created error file to handle server error

--- server.js
+++ server.js
@@ -1,39 +1,29 @@
-const express = require('express');
-const path = require('path');
+import express from 'express';
+import path from 'path';
+import posts from './routes/posts.js';
+import logger from './middleware/logger.js';
+import errorHandler from './middleware/error.js';
 const port = process.env.PORT || 8000
 
 const app = express();
 
+// Bodyparser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }))
+
+// logger mideeleware
+app.use(logger)
+
 // setup static folder
 // app.use(express.static(path.join(__dirname, 'public')))
 
-let posts = [
-    { id: 1, title: "Post One" },
-    { id: 2, title: "Post Two" },
-    { id: 3, title: "Post Three" }
-
-]
 
 
-// get all posts
-app.get('/api/posts', (req, res) => {
-   res.json(posts)
-})
+// Routes
+app.use('/api/posts', posts)
 
-
-// get single post
-app.get('/api/posts/:id', (req, res) => {
-    const id = parseInt(req.params.id);
-    const post = posts.find((post) => post.id === id )
-
-    if (post){ 
-        return res
-        .status(200)
-        .json(post)
-    }
-        res.json({msg: "This post does not exist"})
-
-})
+// Error Handler 
+app.use(errorHandler)
 
 app.listen(port, () => console.log(`Server is running on port ${port}`))
 
