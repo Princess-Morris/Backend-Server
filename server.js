ff32e32d8b5a32c7d5547d6ef47bda42ca538cf5
Princess-Morris <okpataprincessmorris3@gmail.com>
2025-02-24 11:15:45 +0100

    created a default 404 for not-existing route

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ import path from 'path';
 import posts from './routes/posts.js';
 import logger from './middleware/logger.js';
 import errorHandler from './middleware/error.js';
+import notFound from './middleware/notFound.js';
+
 const port = process.env.PORT || 8000
 
 const app = express();
@@ -17,12 +19,11 @@ app.use(logger)
 // setup static folder
 // app.use(express.static(path.join(__dirname, 'public')))
 
-
-
 // Routes
 app.use('/api/posts', posts)
 
 // Error Handler 
+app.use(notFound)
 app.use(errorHandler)
 
 app.listen(port, () => console.log(`Server is running on port ${port}`))
